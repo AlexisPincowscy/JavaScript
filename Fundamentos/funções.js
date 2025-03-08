fb05b9c653d48f8dadb85489a732d4ecb6412575
AlexisPincowscy <alexispincowscy@hotmail.com>
2025-03-08 14:38:42 -0300

    atualização aula funções

--- Fundamentos/funções.js
+++ Fundamentos/funções.js
@@ -14,3 +14,31 @@ console.log(resultadoMediaNotas)
 // PODIA TER COLCOADO A FUNÇÃO DIRETO NO LOG
 // AQUI COM OUTRAS NOTAS COMO PARAMÊTRO
 console.log(mediaNotas(5,1,10))
+
+// Outras opçoes de escrita das funlções
+
+// A literal
+function conta (a,b){
+    console.log(a+b)
+}
+
+conta (1,5)
+
+// A anônima, um valor já recebe a função
+let soma = function(a,b){
+    console.log(a+b)
+}
+
+soma(2,8)
+
+// Arrow Function, some a palavra functiona da anônima
+let sub = (a,b) =>{
+    console.log(a - b)
+}
+
+sub(8,4)
+
+// Arrow retorno implícito, quando temos um comando só
+// tiramos os parênteses, mas só cabe uma linha de comando
+let multi = (a,b) => console.log(a*b)
+multi(2,5)
