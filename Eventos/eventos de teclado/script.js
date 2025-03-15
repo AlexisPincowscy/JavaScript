089b03a7337d131f4683641f3a5f002f31051001
AlexisPincowscy <alexispincowscy@hotmail.com>
2025-03-15 19:18:43 -0300

    feat: aula de Focus e Blur

--- Eventos/eventos de teclado/script.js
+++ Eventos/eventos de teclado/script.js
@@ -1,31 +1,12 @@
 function executar(){
     console.log('Executou!')
-    
-}
-function duplo(){
-    console.log('CLique duplo!')
-    
-}function mouseOver(){
-    console.log('Mouse em cima!')
-    
-}
-
-function mouseOut(){
-    console.log('Mouse fora!')
-    
+    foco.focus()//trava o objeto, da foco ali
 }
 
 
-const entrada = document.getElementById('butao')
-const entrada2 = document.getElementById('h1')
-// entrada.addEventListener('click',executar) // executa quando clicamos, na verdade quando solta o botão do mouse
-// // serve em diversos elemento, podemos clicar num H1
+// método útil para validações de formulário
 
-// entrada2.addEventListener('click',executar)
-// entrada.addEventListener('click',executar) 
-entrada.addEventListener('dblclick',duplo) // executa no double click, podendo ser usado junto com o outro
-// entrada.addEventListener('mousedown',executar) // quando pressiona ele executa
-// entrada.addEventListener('mouseup',executar) // quando vc libera o botçao do mouse
-// entrada.addEventListener('mousemove',executar) //vai executando enquanto o mouse está por cima do elemento
-entrada.addEventListener('mouseover',mouseOver) //quando o cursos passa por cima, conta uma vez a cada passagem
-entrada.addEventListener('mouseout',mouseOut)// quando o cursos sai de cima do elemento
+const foco = document.getElementById('foco')//serve para travar o foco no elemento
+const blur = document.getElementById('blur')
+
+foco.addEventListener('blur',executar)//quando ele tenta sair, desficar, ele volta pro foco
