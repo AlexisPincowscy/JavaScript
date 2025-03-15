e4ed6ef10f0df7b2946eb69baa0d9500b044e3f0
AlexisPincowscy <alexispincowscy@hotmail.com>
2025-03-15 18:45:35 -0300

    feat: aula de eventos de interveção do usuário

--- Eventos/Interface do usuário/script.js
+++ Eventos/Interface do usuário/script.js
@@ -1,32 +1,13 @@
 function executar(){
     console.log('Executou!')
-    botao.removeEventListener('click', executar)//aqui ele vai executar e aprimeria vez e travar nas próximas, só vai valer o primeri click
+    
 }
 
-// const botao = document.getElementById('botao')
-// botao.onclick = executar
 
+// Aula de ações quando o usuário faz algumas dessas ações:
 
-// versão usada quando não se tem id ou outra classificação fácil para o elemento
-// const botao = document.querySelector('[botao-acao]') // usar o querySelector com [] pega o atributo
-// botao.onclick = executar
-// botao.onnclick = function(){executar()} trb funciona do modo anônimo
-
-const botao = document.querySelector('[botao-acao]')
-
-// com addEventListener os parâmetro são ('evento',função,bool)
-// o parâmetro booleano tem padrão False e serve para conttrolar a ordem de ações
-// se tiver no padrão ele vai executar primeiro o evento menor e ir expandindo (evento bota e evento no body)
-// se colcoarmos como true, ele inverte essa ordem
-
-botao.addEventListener('click', executar)
-// podemos chamar direto como função anônima
-// botao.addEventListener('click', function(){executar()}) pode ter mais de uma função sendo executada dentro
-
-// em algumas versão antigas do IExplorer não funciona o addEventListener, então é usado o attachEvent (mesmos parâmetros)
-// podemos fazer uma verificação para tal
-// if (botao.addEventListener){
-//     botao.addEventListener('click', executar)
-// }else{
-//     botao.attachEvent('click', executar)
-// }
+// window.addEventListener('load', executar) load é no ato de carregar a página
+// window.addEventListener('unload', executar)aqui é enquanto ela carrega, podemos até colocar uma imgem para aguradar o carregamento
+// window.addEventListener('error', executar) para quando acontecer um erro
+// window.addEventListener('resize', executar) vai executando quando vai mudando o tamanho da tela
+// window.addEventListener('scroll', executar) executa enquanto o usuário mexe o scroll
