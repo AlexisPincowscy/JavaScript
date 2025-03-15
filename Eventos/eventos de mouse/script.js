b137aa1a57dff1933f92a71aad4f5d4464f5a3e2
AlexisPincowscy <alexispincowscy@hotmail.com>
2025-03-15 19:08:59 -0300

    feat: aula de eventos com mouse

---
+++ Eventos/eventos de mouse/script.js
@@ -0,0 +1,9 @@
+function executar(){
+    console.log('Executou!')
+    
+}
+
+const entrada = document.getElementById('entrada')
+// entrada.addEventListener('keydown',executar) executa no click do botão do teclado VALE PARA QUALQUER TECLA
+// entrada.addEventListener('keyup',executar) executa quando tiro o dedo do teclado
+// entrada.addEventListener('keypress',executar) igual o keydown, mas não executa com teclas funcionais (alt, ctrl, etc), só lê teclas com caracteres
--- Eventos/eventos de teclado/script.js
+++ Eventos/eventos de teclado/script.js
@@ -2,8 +2,30 @@ function executar(){
     console.log('Executou!')
     
 }
+function duplo(){
+    console.log('CLique duplo!')
+    
+}function mouseOver(){
+    console.log('Mouse em cima!')
+    
+}
 
-const entrada = document.getElementById('entrada')
-// entrada.addEventListener('keydown',executar) executa no click do botão do teclado VALE PARA QUALQUER TECLA
-// entrada.addEventListener('keyup',executar) executa quando tiro o dedo do teclado
-// entrada.addEventListener('keypress',executar) igual o keydown, mas não executa com teclas funcionais (alt, ctrl, etc), só lê teclas com caracteres
+function mouseOut(){
+    console.log('Mouse fora!')
+    
+}
+
+
+const entrada = document.getElementById('butao')
+const entrada2 = document.getElementById('h1')
+// entrada.addEventListener('click',executar) // executa quando clicamos, na verdade quando solta o botão do mouse
+// // serve em diversos elemento, podemos clicar num H1
+
+// entrada2.addEventListener('click',executar)
+// entrada.addEventListener('click',executar) 
+entrada.addEventListener('dblclick',duplo) // executa no double click, podendo ser usado junto com o outro
+// entrada.addEventListener('mousedown',executar) // quando pressiona ele executa
+// entrada.addEventListener('mouseup',executar) // quando vc libera o botçao do mouse
+// entrada.addEventListener('mousemove',executar) //vai executando enquanto o mouse está por cima do elemento
+entrada.addEventListener('mouseover',mouseOver) //quando o cursos passa por cima, conta uma vez a cada passagem
+entrada.addEventListener('mouseout',mouseOut)// quando o cursos sai de cima do elemento
